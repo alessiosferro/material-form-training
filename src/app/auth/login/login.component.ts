fc4a13a87c13c632f4b731a67ed3715dd850752e
Alessio Sferro <sferro.alessio@gmail.com>
2019-12-30 11:49:54 +0100

    Login form finished

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
   }
 
+  onSubmit() {
+    alert('Form submitted! Form value showed in console.');
+    console.log(this.loginForm.value);
+  }
+
+  getControl(controlName: string) {
+    return this.loginForm.get(controlName) as FormControl;
+  }
 }
